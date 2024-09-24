54a99782651ac6950e45a9c5fa4cd567afe035d1
Mohit Raj <mohitraj12099@gmail.com>
2024-09-24 20:16:11 +0000

    javascript

---
+++ piyush-garg/02/script.js
@@ -0,0 +1,42 @@
+//  Variable
+// Var
+
+var num1 = 10;
+var num2 = 20;
+var num3 = num1+num2;
+console.log(num3);
+
+// firstname;
+// First_Name;
+// first_name;
+// FirstName; => Used for class names
+// firstName; (most popular) => camelCase
+
+// Var has a global scope;
+
+// Let and Const have Local scope;
+
+// Const me variable re assign nhi ho sakti hai;
+
+const me = 'mohit';
+// me = 'raj'===> error
+
+
+// Today we study about the variables in JavaScript;
+
+/* 
+var, let, const
+
+var - Global
+let - Local
+const - Constant
+
+var - re-assign
+let - re-assign
+const - not_re-assign
+*/
+
+if(true){
+    const FirstNAME = 'mohit';
+    console.log(FirstNAME);
+}
---
+++ piyush-garg/03/script.js
@@ -0,0 +1,48 @@
+// data types
+// numbers
+
+let num1 = 34;
+let num2 = 34.5;
+
+// string
+
+const firstname = 'mohit';
+const lastName = 'raj';
+
+console.log(firstname + ' ' + lastName);
+
+// boolean 
+
+let isLoggedIn = true;
+
+console.log(isLoggedIn);
+
+
+// undefined
+
+let lastLoginDate = null;
+console.log(lastLoginDate);
+
+
+// object
+
+const person = {
+    firstname: 'mohit',
+    lastname: 'raj',
+    age: 20,
+    isLoggedIn: true
+
+}
+console.log(person);
+
+
+
+if(0){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+let islogedin = true;
+
+console.log(islogedin + 10);
